bc49ef59d8368f0bda8f2c0b5b28e2c88ac3e4c2
Andrés Efrain Chango Macas <ext_aechango@falabella.cl>
2023-12-16 09:53:14 -0500

    valitade

--- SwiftUI/PropertyWrappers/PropertyWrappers/ContentView.swift
+++ SwiftUI/PropertyWrappers/PropertyWrappers/ContentView.swift
@@ -6,15 +6,25 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     @State private var count = 0
+    @State private var userName  = ""
     var body: some View {
         VStack {
 //            incrbuementar contador
             Button("(\(count))"){
                 count += 1
             }
+            TextField("UserName", text: $userName)
+                .keyboardType(.emailAddress)
+                .onReceive(Just(userName)) { newValue in
+                                let filtered = newValue.filter { "0123456789".contains($0) }
+                                if filtered != newValue {
+                                    self.userName = filtered
+                                }
+                            }
 
         }
         .padding()
